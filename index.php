6b86ad7cccd9d909b2640de6c486bf64f36d7d38
kanazui <bulanokanazara7@gmail.com>
2026-01-16 22:28:17 +0800

    isi_benner

--- index.php
+++ index.php
@@ -114,10 +114,10 @@ $current_page = 'home';
         <div class="row g-4 align-items-center">
             <div class="col-lg-6 px-5">
                 <div class="py-4">
-                    <h1 class="display-3 text-white">Fresh Exotic Fruits</h1>
-                    <p class="fw-normal display-3 text-dark mb-4">in Our Store</p>
-                    <p class="mb-4 text-dark">The generated Lorem Ipsum is therefore always free from repetition injected humour, or non-characteristic words etc.</p>
-                    <a href="shop.php" class="banner-btn btn border-2 border-white rounded-pill text-dark py-3 px-5">BUY</a>
+                    <h1 class="display-3 text-white">Dari Kebun Lokal</h1>
+                    <p class="fw-normal display-3 text-dark mb-4">ke Rumah Anda</p>
+                    <p class="mb-4 text-dark">Mendukung petani lokal sambil menghadirkan produk segar setiap hari tanpa perantara dan harga yang transparan.</p>
+                    <a href="shop.php" class="banner-btn btn border-2 border-white rounded-pill text-dark py-3 px-5">BELI</a>
                 </div>
             </div>
             <div class="col-lg-6 px-5">
