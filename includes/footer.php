4291a1633317f6d1e74004cd23e2ce7a4f60d5b0
NiMadeSulistyawati <madewati057@gmail.com>
2026-01-17 19:33:31 +0700

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,95 +1,78 @@
-
-
-    <!-- Footer Start - FULL WIDTH -->
-    <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 px-0">
-        <div class="container py-5">
-            <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5);">
-                <div class="row g-4 justify-content-center">
-                    <div class="col-lg-3 col-md-6">
-                        <a href="#">
-                            <h1 class="text-primary mb-0">DariKebun</h1>
-                            <p class="text-secondary mb-0">Kesegaran yang Bisa Kamu Percaya</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-5 col-md-6">
-                        <div class="position-relative mx-auto">
-                            <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="email" placeholder="Your Email">
-                            <button type="submit" class="btn btn-primary border-0 py-3 px-5 position-absolute rounded-pill text-white" style="top: 0; right: 0;">Subscribe Now</button>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="d-flex justify-content-end pt-3">
-                            <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i class="fab fa-youtube"></i></a>
-                            <a class="btn btn-outline-secondary btn-md-square rounded-circle" href=""><i class="fab fa-linkedin-in"></i></a>
-                        </div>
+<!-- Footer Start - FULL WIDTH -->
+<div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 px-0">
+    <div class="container py-5">
+        <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5);">
+            <div class="row g-4 justify-content-center align-items-center">
+                <div class="col-lg-4 col-md-6 text-center text-lg-start">
+                    <a href="#">
+                        <h1 class="text-primary mb-0">DariKebun</h1>
+                        <p class="text-secondary mb-0">Kesegaran yang Bisa Kamu Percaya</p>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="position-relative mx-auto">
+                        <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="email" placeholder="Email Anda">
+                        <button type="submit"
+                            class="btn btn-primary border-0 py-3 px-5 position-absolute rounded-pill text-white"
+                            style="top: 0; right: 0;">
+                            Subscribe Sekarang
+                        </button>
                     </div>
                 </div>
-            </div>
-
-            <div class="row g-5 justify-content-center">
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer-item">
-                        <h4 class="text-light mb-3">Why People Like us!</h4>
-                        <p class="mb-4">Kami menyediakan buah dan sayur organik segar langsung dari kebun petani lokal, tanpa pestisida kimia, untuk mendukung gaya hidup sehat Anda.</p>
-                        <a href="about.php" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Read More</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="d-flex flex-column text-start footer-item">
-                        <h4 class="text-light mb-3">Shop Info</h4>
-                        <a class="btn-link" href="about.php">About Us</a>
-                        <a class="btn-link" href="contact.php">Contact Us</a>
-                        <a class="btn-link" href="">Privacy Policy</a>
-                        <a class="btn-link" href="">Terms & Condition</a>
-                        <a class="btn-link" href="">Return Policy</a>
-                        <a class="btn-link" href="">FAQs & Help</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="d-flex flex-column text-start footer-item">
-                        <h4 class="text-light mb-3">Account</h4>
-                        <a class="btn-link" href="">My Account</a>
-                        <a class="btn-link" href="shop.php">Shop Details</a>
-                        <a class="btn-link" href="cart.php">Shopping Cart</a>
-                        <a class="btn-link" href="">Wishlist</a>
-                        <a class="btn-link" href="">Order History</a>
-                        <a class="btn-link" href="">International Orders</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer-item">
-                        <h4 class="text-light mb-3">Contact</h4>
-                        <p>Address: Jl. Soekarno Hatta No. 123, Bandar Lampung</p>
-                        <p>Email: info@darikebun.com</p>
-                        <p>Phone: +62 821 1234 5678</p>
-                        <p>Payment Accepted</p>
-                        <img src="img/payment.png" class="img-fluid" alt="Payment Methods">
+                <div class="col-lg-4 col-md-12">
+                    <div class="d-flex justify-content-center justify-content-lg-end pt-3">
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="https://x.com/ShopeeID"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="https://www.tiktok.com/@shopee_id?_r=1&_t=ZS-938nzBHMlpo"><i class="fab fa-tiktok"></i></a>
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="https://youtube.com/@shopeeindonesia?si=ot1BXzY4XNqYDNKm"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-secondary btn-md-square rounded-circle" href="https://www.instagram.com/shopee_id?igsh=MTU1Z2V6ZXB3Mmwweg=="><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
         </div>
+
+        <!-- Footer Content 3 Kolom -->
+        <div class="row g-5 justify-content-center">
+            <div class="col-lg-4 col-md-6">
+                <div class="footer-item">
+                    <h4 class="text-light mb-3">Mengapa Kami Disukai!</h4>
+                    <p class="mb-4">
+                        Kami menyediakan buah dan sayur organik segar langsung dari kebun petani lokal,
+                        tanpa pestisida kimia, untuk mendukung gaya hidup sehat Anda.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="footer-item mx-auto" style="max-width: 220px; margin-left: 12px;">
+                    <h4 class="text-light mb-3 text-start">Ekstra Link</h4>
+
+                    <div class="d-flex flex-column gap-2 text-start">
+                        <a class="btn-link" href="index.php">Beranda</a>
+                        <a class="btn-link" href="orders.php">Pesanan</a>
+                        <a class="btn-link" href="cart.php">Keranjang</a>
+                        <a class="btn-link" href="contact.php">Kontak</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="footer-item">
+                    <h4 class="text-light mb-3">Kontak</h4>
+                    <p>Alamat: Jl. Soekarno Hatta No. 123, Bandar Lampung</p>
+                    <p>Email: info@darikebun.com</p>
+                    <p>Telepon: +62 821 1234 5678</p>
+                    <p>Pembayaran yang Tersedia</p>
+                    <img src="img/payment.png" class="img-fluid" alt="Payment Methods">
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+<!-- Footer End -->
+
     <!-- Footer End -->
 
-    <!-- Copyright Start - FULL WIDTH -->
-    <div class="container-fluid copyright bg-dark py-4 px-0">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    <span class="text-light">
-                        <a href="#"><i class="fas fa-copyright text-light me-2"></i>DariKebun</a>, All rights reserved.
-                    </span>
-                </div>
-                <div class="col-md-6 text-center text-md-end text-white">
-                    Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a> 
-                    Distributed By <a class="border-bottom" href="https://themewagon.com">ThemeWagon</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Copyright End -->
+
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-primary border-3 border-primary rounded-circle back-to-top">
