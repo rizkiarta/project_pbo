793e41262d04fe748765fcb3c40ed0ef14b1569d
NiMadeSulistyawati <madewati057@gmail.com>
2026-01-17 16:08:20 +0700

    cart

--- cart.php
+++ cart.php
@@ -1,140 +1,125 @@
 <?php
-require_once 'includes/config.php';
+// cart.php - FULL VERSION (User Interface)
+require_once 'includes/functions.php';
 require_once 'includes/head.php';
-require_once 'includes/spinner.php';
-$current_page = 'shop';
-require_once 'includes/navbar.php';
+
+// Ambil data keranjang terbaru dari database
+$items = getCartItems();
+$subtotal = getCartTotal();
+$shipping = getShippingFee();
+$grand_total = getGrandTotal();
 ?>
 
 <body>
-
-    <div class="container-fluid page-header py-5 mb-5">
-        <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">Keranjang</h1>
-            <nav aria-label="breadcrumb animated slideInDown">
-                <ol class="breadcrumb text-uppercase">
-                    <li class="breadcrumb-item"><a href="index.php" class="text-white">Beranda</a></li>
-                    <li class="breadcrumb-item text-white active" aria-current="page">Keranjang</li>
-                </ol>
-            </nav>
-        </div>
+    <div class="container-fluid page-header py-5">
+        <h1 class="text-center text-white display-6">Keranjang Belanja</h1>
+        <ol class="breadcrumb justify-content-center mb-0">
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item active text-white">Cart</li>
+        </ol>
     </div>
+
     <div class="container-fluid py-5">
         <div class="container py-5">
             <div class="table-responsive">
+                
+                <?php if (empty($items)): ?>
+                    <div class="text-center py-5">
+                        <h3>Keranjangmu masih kosong 😢</h3>
+                        <p>Yuk belanja sayur segar dulu!</p>
+                        <a href="index.php" class="btn btn-primary rounded-pill py-3 px-5">Belanja Sekarang</a>
+                    </div>
+                <?php else: ?>
+                
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">Gambar</th>
-                            <th scope="col">Nama Produk</th>
+                            <th scope="col">Produk</th>
+                            <th scope="col">Nama</th>
                             <th scope="col">Harga</th>
                             <th scope="col">Jumlah</th>
                             <th scope="col">Total</th>
+                            <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php
-                        $cart_items = getCartItems(); // Dari functions.php
-                        $cart_total = getCartTotal();
-                        $shipping = getShippingFee();
-                        $grand_total = getGrandTotal();
-
-                        if (empty($cart_items)): ?>
-                            <tr>
-                                <td colspan="6" class="text-center py-5">
-                                    <h5>Keranjang belanja kosong</h5>
-                                    <a href="index.php" class="btn border-secondary rounded-pill px-4 py-3 text-primary mt-3">
-                                        Mulai Belanja
-                                    </a>
-                                </td>
-                            </tr>
-                        <?php else: ?>
-                            <?php foreach ($cart_items as $item): ?>
-                                <tr>
-                                    <th scope="row">
-                                        <div class="d-flex align-items-center">
-                                            <img src="<?php echo htmlspecialchars($item['image']); ?>"
-                                                class="img-fluid me-5 rounded-circle"
-                                                style="width: 80px; height: 80px;"
-                                                alt="<?php echo htmlspecialchars($item['name']); ?>">
-                                        </div>
-                                    </th>
-                                    <td>
-                                        <p class="mb-0 mt-4 fw-bold"><?php echo htmlspecialchars($item['name']); ?></p>
-                                    </td>
-                                    <td>
-                                        <p class="mb-0 mt-4">Rp <?php echo number_format($item['price']); ?></p>
-                                    </td>
-                                    <td>
-                                        <div class="input-group quantity mt-4" style="width: 100px;">
-                                            
-                                            <div class="input-group-btn">
-                                                <a href="update_quantity.php?id=<?php echo $item['id']; ?>&action=decrease" 
-                                                   class="btn btn-sm btn-minus rounded-circle bg-light border">
-                                                    <i class="fa fa-minus"></i>
-                                                </a>
-                                            </div>
-
-                                            <input type="text" class="form-control form-control-sm text-center border-0 quantity-display"
-                                                value="<?php echo $item['quantity']; ?>" readonly>
-
-                                            <div class="input-group-btn">
-                                                <a href="update_quantity.php?id=<?php echo $item['id']; ?>&action=increase" 
-                                                   class="btn btn-sm btn-plus rounded-circle bg-light border">
-                                                    <i class="fa fa-plus"></i>
-                                                </a>
-                                            </div>
-
-                                        </div>
-                                    </td>
-
-                                    <td class="mb-0 mt-4 fw-bold subtotal-display">
-                                        Rp <?php echo number_format($item['subtotal']); ?>
-                                    </td>
-                                    <td>
-                                        <a href="remove_from_cart.php?id=<?php echo $item['id']; ?>"
-                                            class="btn btn-md rounded-circle bg-light border mt-4">
-                                            <i class="fa fa-times text-danger"></i>
+                        <?php foreach ($items as $item): ?>
+                        <tr>
+                            <td scope="row">
+                                <div class="d-flex align-items-center">
+                                    <img src="img/<?php echo $item['image']; ?>" class="img-fluid me-5 rounded-circle" style="width: 80px; height: 80px;" alt="">
+                                </div>
+                            </td>
+                            
+                            <td>
+                                <p class="mb-0 mt-4"><?php echo $item['name']; ?></p>
+                            </td>
+                            
+                            <td>
+                                <p class="mb-0 mt-4">Rp <?php echo number_format($item['price']); ?></p>
+                            </td>
+                            
+                            <td>
+                                <div class="input-group quantity mt-4" style="width: 100px;">
+                                    <div class="input-group-btn">
+                                        <a href="update_quantity.php?id=<?php echo $item['product_id']; ?>&action=decrease" class="btn btn-sm btn-minus rounded-circle bg-light border">
+                                            <i class="fa fa-minus"></i>
                                         </a>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        <?php endif; ?>
+                                    </div>
+                                    
+                                    <input type="text" class="form-control form-control-sm text-center border-0" value="<?php echo $item['quantity']; ?>" readonly>
+                                    
+                                    <div class="input-group-btn">
+                                        <a href="update_quantity.php?id=<?php echo $item['product_id']; ?>&action=increase" class="btn btn-sm btn-plus rounded-circle bg-light border">
+                                            <i class="fa fa-plus"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </td>
+                            
+                            <td>
+                                <p class="mb-0 mt-4">Rp <?php echo number_format($item['subtotal']); ?></p>
+                            </td>
+                            
+                            <td>
+                                <a href="remove_from_cart.php?id=<?php echo $item['id']; ?>" class="btn btn-md rounded-circle bg-light border mt-4" onclick="return confirm('Yakin mau hapus?')">
+                                    <i class="fa fa-times text-danger"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
-            </div>
-            <div class="row g-4 justify-content-end">
-                <div class="col-8"></div>
-                <div class="col-sm-8 col-md-7 col-lg-6 col-xl-4">
-                    <div class="bg-light rounded">
-                        <div class="p-4">
-                            <h1 class="display-6 mb-4">Total <span class="fw-normal">Keranjang</span></h1>
-                            <div class="d-flex justify-content-between mb-4">
-                                <h5 class="mb-0 me-4">Subtotal:</h5>
-                                <p class="mb-0">Rp <?php echo number_format($cart_total); ?></p>
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <h5 class="mb-0 me-4">Ongkos Kirim</h5>
-                                <div>
-                                    <p class="mb-0">Tarif Tetap: Rp <?php echo number_format($shipping); ?></p>
+                
+                <div class="row g-4 justify-content-end">
+                    <div class="col-8"></div>
+                    <div class="col-sm-8 col-md-7 col-lg-6 col-xl-4">
+                        <div class="bg-light rounded">
+                            <div class="p-4">
+                                <h1 class="display-6 mb-4">Total <span class="fw-normal">Belanja</span></h1>
+                                <div class="d-flex justify-content-between mb-4">
+                                    <h5 class="mb-0 me-4">Subtotal:</h5>
+                                    <p class="mb-0">Rp <?php echo number_format($subtotal); ?></p>
+                                </div>
+                                <div class="d-flex justify-content-between">
+                                    <h5 class="mb-0 me-4">Ongkir:</h5>
+                                    <div class="">
+                                        <p class="mb-0">Rp <?php echo number_format($shipping); ?></p>
+                                    </div>
                                 </div>
                             </div>
-                            <hr>
-                            <div class="d-flex justify-content-between">
-                                <h5 class="mb-0 me-4">Total:</h5>
-                                <p class="mb-0 fw-bold">Rp <?php echo number_format($grand_total); ?></p>
+                            <div class="py-4 mb-4 border-top border-bottom d-flex justify-content-between">
+                                <h5 class="mb-0 ps-4 me-4">Grand Total</h5>
+                                <p class="mb-0 pe-4">Rp <?php echo number_format($grand_total); ?></p>
                             </div>
-                        </div>
-                        <div class="py-4 text-center">
-                            <a href="checkout.php" class="btn border-secondary rounded-pill px-4 py-3 text-primary text-uppercase w-100">
-                                Lanjut ke Pembayaran
-                            </a>
+                            <a href="checkout.php" class="btn btn-primary border-secondary rounded-pill px-4 py-3 text-primary text-uppercase mb-4 ms-4" type="button">Checkout</a>
                         </div>
                     </div>
                 </div>
+
+                <?php endif; ?>
             </div>
         </div>
     </div>
-    <?php
-    require_once 'includes/footer.php';
-    ?>
+
+<?php require_once 'includes/footer.php'; ?>
--- update_quantity.php
+++ update_quantity.php
@@ -1,37 +1,29 @@
 <?php
-// update_quantity.php - SUDAH DIPERBAIKI (Versi $connect)
+// update_quantity.php - FULL VERSION
+require_once 'includes/functions.php';
 
-// Kita panggil config.php dulu. 
-// File ini sudah otomatis menjalankan session_start() dan membuat koneksi database ($connect)
-require_once 'includes/config.php';
-
-// Pastikan user sudah login
+// 1. Cek Login
 if (!isset($_SESSION['user_id'])) {
-    // Kalau belum login, lempar ke halaman login (opsional)
     header("Location: login.php");
     exit;
 }
 
-// Tangkap data dari URL
+// 2. Tangkap Data
 if (isset($_GET['id']) && isset($_GET['action'])) {
     $product_id = (int)$_GET['id'];
     $action     = $_GET['action'];
-    $user_id    = (int)$_SESSION['user_id']; // Ambil ID user dari session
+    $user_id    = (int)$_SESSION['user_id'];
 
-    // -----------------------------------------------------------
-    // Menggunakan variabel $connect (sesuai config.php kamu)
-    // -----------------------------------------------------------
-
-    // 1. Ambil jumlah (quantity) saat ini dari Database
-    // Asumsi nama tabel: 'cart' (jika masih error, cek nama tabel di database)
-    $query = "SELECT quantity FROM cart WHERE user_id = '$user_id' AND product_id = '$product_id'";
-    $result = mysqli_query($connect, $query);
+    // 3. Ambil data keranjang saat ini
+    $query = "SELECT id, quantity FROM cart WHERE user_id = '$user_id' AND product_id = '$product_id'";
+    $result = mysqli_query($conn, $query);
 
     if ($result && mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
         $current_qty = (int)$row['quantity'];
+        $cart_id = $row['id'];
         
-        // 2. Hitung Quantity Baru
+        // 4. Hitung Matematika
         if ($action == 'increase') {
             $new_qty = $current_qty + 1;
         } elseif ($action == 'decrease') {
@@ -40,22 +32,20 @@ if (isset($_GET['id']) && isset($_GET['action'])) {
             $new_qty = $current_qty;
         }
 
-        // 3. Update ke Database
+        // 5. Simpan ke Database
         if ($new_qty > 0) {
-            // Update angka baru
-            $update_query = "UPDATE cart SET quantity = '$new_qty' WHERE user_id = '$user_id' AND product_id = '$product_id'";
-            mysqli_query($connect, $update_query);
+            // Update jumlah baru
+            $update_query = "UPDATE cart SET quantity = '$new_qty' WHERE id = '$cart_id'";
+            mysqli_query($conn, $update_query);
         } else {
-            // LOGIKA PENGHAPUSAN/BATAS MINIMAL
-            // Jika jumlah jadi 0, kita set mentok di 1 saja.
-            // (Kalau mau dihapus otomatis, ganti query ini dengan DELETE)
-            $update_query = "UPDATE cart SET quantity = '1' WHERE user_id = '$user_id' AND product_id = '$product_id'";
-            mysqli_query($connect, $update_query);
+            // Kalau 0, hapus barangnya
+            $delete_query = "DELETE FROM cart WHERE id = '$cart_id'";
+            mysqli_query($conn, $delete_query);
         }
     }
 }
 
-// 4. Redirect kembali ke halaman cart
+// 6. Refresh Halaman Cart (Supaya harga & notif kuning update)
 header("Location: cart.php");
 exit;
 ?>
