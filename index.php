2e15e7ee53c7adcb78c5ca60655b4a7a293b8193
Fina <vlioreta05@gmail.com>
2026-01-17 13:14:33 +0700

    nambah client testimonial

--- index.php
+++ index.php
@@ -233,6 +233,7 @@ $current_page = 'home';
         </div>
         <div class="owl-carousel testimonial-carousel">
             <!-- Testimonial items (sama seperti asli) -->
+             <!-- testimonial item 1 -->
             <div class="testimonial-item img-border-radius bg-light rounded p-4 mx-3">
                 <div class="position-relative">
                     <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
@@ -283,7 +284,57 @@ $current_page = 'home';
                     </div>
                 </div>
             </div>
-            
+              <!-- Tambahkan item testimonial lain kalau ada -->
+                <div class="testimonial-item img-border-radius bg-light rounded p-4 mx-3">
+                <div class="position-relative">
+                    <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                    <div class="mb-4 pb-4 border-bottom border-secondary">
+                        <p class="mb-0">“Saya pesan apel dan jeruk, semuanya segar dan berkualitas. Anak-anak di sekolah juga suka.”</p>
+                    </div>
+                    <div class="d-flex align-items-center flex-nowrap">
+                        <div class="bg-secondary rounded">
+                            <img src="img/Testimonial/Client3.jpg" class="img-fluid rounded" style="width: 100px; height: 120px;" alt="">
+                        </div>
+                        <div class="ms-4 d-block">
+                            <h4 class="text-dark">Dewi Lestari</h4>
+                            <p class="m-0 pb-3">Guru</p>
+                            <div class="d-flex pe-5">
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Tambahkan item testimonial lain kalau ada -->
+              <div class="testimonial-item img-border-radius bg-light rounded p-4 mx-3">
+                <div class="position-relative">
+                    <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                    <div class="mb-4 pb-4 border-bottom border-secondary">
+                        <p class="mb-0">“Harga terjangkau untuk kualitas sebagus ini. Cocok buat anak kos yang mau hidup lebih sehat.”</p>
+                    </div>
+                    <div class="d-flex align-items-center flex-nowrap">
+                        <div class="bg-secondary rounded">
+                            <img src="img/Testimonial/Client4.jpg" class="img-fluid rounded" style="width: 100px; height: 120px;" alt="">
+                        </div>
+                        <div class="ms-4 d-block">
+                            <h4 class="text-dark">Bagus Pratama</h4>
+                            <p class="m-0 pb-3">Mahasiswa</p>
+                            <div class="d-flex pe-5">
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                                <i class="fas fa-star text-primary"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Tambahkan item testimonial lain kalau ada -->
         </div>
     </div>
 </div>
